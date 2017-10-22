748676a86afbf1cb47c0b1bba9ce4e61213c6765
dk54dk54 <32596227+dk54dk54@users.noreply.github.com>
2017-10-22 16:28:56 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Display "Hello world!" on the monitor
+
+public class HelloWorld {
+
+   public static void main(String[ ] args) {
+   
+      System.out.println("Hello world!");
+      
+   } // End main
+} // End class 
